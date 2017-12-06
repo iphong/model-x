499faf403d4e87adfb47f26efae33d5a9771f163
Phong Vu <phong.vu@me.com>
2017-12-07 00:05:09 +0700

    Add nested events triggering

--- test/model.spec.js
+++ test/model.spec.js
@@ -1,36 +1,36 @@
 const { change, update, observable, observe, intercept } = require('../dist')
 
-const state = {}
-const observer = id => async (key, next, prev) => {
-	await new Promise(resolve => {
-		setTimeout(() => {
-			console.log(`[${id}]`, key, ' --> ', next)
-			resolve()
-		}, 500)
-	})
-}
-intercept(state, 'foo', value => `foo( ${value} )`)
-intercept(state, 'bar', value => `bar( ${value} )`)
-intercept(state, value => `$$( ${value} )`)
-
-observe(state, observer(1))
-observe(state, observer(2))
-
-update(state, { foo: 'something' }).then(() => {
-	console.log('')
-	console.log('---  updated "foo"', state.foo === 'something')
-	console.log('')
-})
-update(state, { bar: 'another' }).then(() => {
-	console.log('')
-	console.log('---  updated "bar"', state.bar === 'another')
-	console.log('')
-})
-update(state, { bar: 'new value' }).then(() => {
-	console.log('')
-	console.log('---  updated "bar"', state.bar === 'new value')
-	console.log('')
-})
+// const state = {}
+// const observer = id => async (key, next, prev) => {
+// 	await new Promise(resolve => {
+// 		setTimeout(() => {
+// 			console.log(`[${id}]`, key, ' --> ', next)
+// 			resolve()
+// 		}, 500)
+// 	})
+// }
+// intercept(state, 'foo', value => `foo( ${value} )`)
+// intercept(state, 'bar', value => `bar( ${value} )`)
+// intercept(state, value => `$$( ${value} )`)
+//
+// observe(state, observer(1))
+// observe(state, observer(2))
+//
+// update(state, { foo: 'something' }).then(() => {
+// 	console.log('')
+// 	console.log('---  updated "foo"', state.foo === 'something')
+// 	console.log('')
+// })
+// update(state, { bar: 'another' }).then(() => {
+// 	console.log('')
+// 	console.log('---  updated "bar"', state.bar === 'another')
+// 	console.log('')
+// })
+// update(state, { bar: 'new value' }).then(() => {
+// 	console.log('')
+// 	console.log('---  updated "bar"', state.bar === 'new value')
+// 	console.log('')
+// })
 
 
 const app = observable({ a: { b: { c: 1 } } })
